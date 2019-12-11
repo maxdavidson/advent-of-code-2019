7384883f1dcd6ee272669441a741483e15e75aef
Max Davidson <max.davidson@cygni.se>
2019-12-11 23:42:57 +0100

    Add solutions for day 10

---
+++ src/day10/mod.rs
@@ -0,0 +1,145 @@
+use crate::utils::point::Point;
+use num::{integer::gcd, Integer, Signed, ToPrimitive};
+use std::cmp;
+use std::collections::{BTreeMap, HashSet, VecDeque};
+use std::hash::Hash;
+use std::iter;
+
+fn parse(input: &str) -> impl Iterator<Item = Point<i16>> + '_ {
+    input.trim().lines().enumerate().flat_map(|(y, line)| {
+        line.chars().enumerate().filter_map(move |(x, c)| match c {
+            '#' => Some(Point(x as i16, y as i16)),
+            _ => None,
+        })
+    })
+}
+
+impl<T: ToPrimitive + Copy> Point<T> {
+    fn to_f64(self) -> Option<Point<f64>> {
+        Some(Point(self.0.to_f64()?, self.1.to_f64()?))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Direction<T>(Point<T>);
+
+impl<T: Integer + Copy> Direction<T> {
+    pub fn from_point(point: Point<T>) -> Option<Direction<T>> {
+        let Point(x, y) = point;
+        if x.is_zero() && y.is_zero() {
+            None
+        } else {
+            let divisor = gcd(x, y);
+            Some(Direction(Point(x / divisor, y / divisor)))
+        }
+    }
+}
+
+impl<T: ToPrimitive + Copy> Direction<T> {
+    fn angle(&self) -> f64 {
+        self.0.to_f64().unwrap().angle(Point(0.0, 1.0))
+    }
+}
+
+impl<T: ToPrimitive + Copy + PartialOrd> PartialOrd for Direction<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        other.angle().partial_cmp(&self.angle())
+    }
+}
+
+impl<T: ToPrimitive + Copy + Eq + PartialOrd> Ord for Direction<T> {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.partial_cmp(other).unwrap_or(cmp::Ordering::Equal)
+    }
+}
+
+fn find_best_monitoring_station<'a, T: Integer + Signed + Hash + Copy + 'a>(
+    asteroids: impl Iterator<Item = &'a Point<T>> + Clone,
+) -> Option<(Point<T>, usize)> {
+    asteroids
+        .clone()
+        .map(|a0| {
+            let directions: HashSet<_> =
+                asteroids.clone().filter_map(|a1| Direction::from_point(*a1 - *a0)).collect();
+
+            (*a0, directions.len())
+        })
+        .max_by_key(|(_, count)| *count)
+}
+
+#[allow(dead_code)]
+fn part1(input: &str) -> (Point<i16>, usize) {
+    let asteroids: Vec<_> = parse(input).collect();
+
+    find_best_monitoring_station(asteroids.iter()).expect("No best asteroid found!")
+}
+
+#[allow(dead_code)]
+fn part2(input: &str) -> Option<i16> {
+    let (best_asteroid, offset_groups) = {
+        let asteroids: Vec<_> = parse(input).collect();
+        let (best_asteroid, _) = find_best_monitoring_station(asteroids.iter()).unwrap();
+
+        let mut seen = BTreeMap::new();
+
+        for asteroid in asteroids {
+            let diff = asteroid - best_asteroid;
+            if let Some(direction) = Direction::from_point(diff) {
+                seen.entry(direction).or_insert_with(Vec::new).push(diff);
+            }
+        }
+
+        let offset_groups: Vec<_> = seen
+            .into_iter()
+            .map(|(_, mut v)| {
+                v.sort_by_cached_key(|v| v.len_sqr());
+                VecDeque::from(v)
+            })
+            .collect();
+
+        (best_asteroid, offset_groups)
+    };
+
+    let shoot_asteroids = move || {
+        let mut group_index = 0;
+        let mut offset_groups = offset_groups;
+        iter::from_fn(move || {
+            let offset_group = offset_groups.get_mut(group_index)?;
+            let offset = offset_group.pop_front()?;
+
+            if offset_group.is_empty() {
+                offset_groups.remove(group_index);
+            } else {
+                group_index = (group_index + 1) % offset_groups.len();
+            }
+
+            Some(offset + best_asteroid)
+        })
+    };
+
+    let Point(x, y) = shoot_asteroids().nth(199)?;
+
+    Some(x * 100 + y)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(part1(include_str!("test_input0")), (Point(3, 4), 8));
+        assert_eq!(part1(include_str!("test_input1")), (Point(5, 8), 33));
+        assert_eq!(part1(include_str!("test_input2")), (Point(1, 2), 35));
+        assert_eq!(part1(include_str!("test_input3")), (Point(6, 3), 41));
+        assert_eq!(part1(include_str!("test_input4")), (Point(11, 13), 210));
+        assert_eq!(part1(include_str!("input")), (Point(11, 11), 221));
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(part2(include_str!("test_input0")), None);
+        assert_eq!(part2(include_str!("test_input4")), Some(802));
+        assert_eq!(part2(include_str!("input")), Some(806));
+    }
+}
--- src/utils/point.rs
+++ src/utils/point.rs
@@ -1,31 +1,46 @@
-use num::{Num, Signed, Zero};
+use num::{Float, Zero};
 use std::ops;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Point<T: Num>(pub T, pub T);
+pub struct Point<T>(pub T, pub T);
 
-impl<T: Num + Zero> Default for Point<T> {
-    fn default() -> Point<T> {
+impl<T: ops::Add<Output = T> + ops::Mul<Output = T> + Copy> Point<T> {
+    pub fn dot(self, other: Point<T>) -> T {
+        self.0 * other.0 + self.1 * other.1
+    }
+}
+
+impl<T: ops::Add<Output = T> + ops::Mul<Output = T> + Copy> Point<T> {
+    pub fn len_sqr(self) -> T {
+        self.0 * self.0 + self.1 * self.1
+    }
+}
+
+impl<T: ops::Sub<Output = T> + ops::Mul<Output = T> + Copy> Point<T> {
+    pub fn cross(self, other: Point<T>) -> T {
+        self.0 * other.1 - self.1 * other.0
+    }
+}
+
+impl<T: Float + Copy> Point<T> {
+    pub fn angle(self, other: Point<T>) -> T {
+        self.cross(other).atan2(self.dot(other))
+    }
+}
+
+impl<T: Zero> Point<T> {
+    pub fn origin() -> Point<T> {
         Point(T::zero(), T::zero())
     }
 }
 
-impl<T: Num> ops::Add for Point<T> {
-    type Output = Point<T>;
-
-    fn add(self, rhs: Point<T>) -> Point<T> {
-        Point(self.0 + rhs.0, self.1 + rhs.1)
+impl<T: Zero> Default for Point<T> {
+    fn default() -> Point<T> {
+        Self::origin()
     }
 }
 
-impl<T: Num + Copy> ops::AddAssign for Point<T> {
-    fn add_assign(&mut self, rhs: Point<T>) {
-        self.0 = self.0 + rhs.0;
-        self.1 = self.1 + rhs.1;
-    }
-}
-
-impl<T: Num + Copy> ops::Mul<T> for Point<T> {
+impl<T: ops::Mul<Output = T> + Copy> ops::Mul<T> for Point<T> {
     type Output = Point<T>;
 
     fn mul(self, rhs: T) -> Point<T> {
@@ -33,7 +48,55 @@ impl<T: Num + Copy> ops::Mul<T> for Point<T> {
     }
 }
 
-impl<T: Num + Signed> ops::Neg for Point<T> {
+impl<T: ops::Mul<Output = T> + Copy> ops::MulAssign<T> for Point<T> {
+    fn mul_assign(&mut self, rhs: T) {
+        *self = *self * rhs;
+    }
+}
+
+impl<T: ops::Div<Output = T> + Copy> ops::Div<T> for Point<T> {
+    type Output = Point<T>;
+
+    fn div(self, rhs: T) -> Point<T> {
+        Point(self.0 / rhs, self.1 / rhs)
+    }
+}
+
+impl<T: ops::Div<Output = T> + Copy> ops::DivAssign<T> for Point<T> {
+    fn div_assign(&mut self, rhs: T) {
+        *self = *self / rhs;
+    }
+}
+
+impl<T: ops::Add<Output = T>> ops::Add for Point<T> {
+    type Output = Point<T>;
+
+    fn add(self, rhs: Point<T>) -> Point<T> {
+        Point(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl<T: ops::Add<Output = T> + Copy> ops::AddAssign for Point<T> {
+    fn add_assign(&mut self, rhs: Point<T>) {
+        *self = *self + rhs;
+    }
+}
+
+impl<T: ops::Sub<Output = T>> ops::Sub for Point<T> {
+    type Output = Point<T>;
+
+    fn sub(self, rhs: Point<T>) -> Point<T> {
+        Point(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl<T: ops::Sub<Output = T> + Copy> ops::SubAssign for Point<T> {
+    fn sub_assign(&mut self, rhs: Point<T>) {
+        *self = *self - rhs;
+    }
+}
+
+impl<T: ops::Neg<Output = T>> ops::Neg for Point<T> {
     type Output = Point<T>;
 
     fn neg(self) -> Point<T> {
